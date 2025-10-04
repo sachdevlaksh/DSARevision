664089e1291e0c074df778b0d5c6ab2b8cd8ee0a
Lakshay Sachdeva <lsachdeva@victoria.com>
2025-10-04 15:43:49 +0530

    updated todays problem

--- Pattern1/LC11/ContainerWithMostWater.go
+++ Pattern1/LC11/ContainerWithMostWater.go
@@ -2,31 +2,15 @@ package LC11
 
 func maxArea(height []int) int {
     i := 0
-    j:= len(height) -1
+    j := len(height) - 1
     maxArea := 0
     for i < j {
-        maxArea = findMax(maxArea,findMin(height[i],height[j])*(j-i))
-        if height[i]< height[j]{
+        maxArea = max(maxArea, min(height[i], height[j])*(j-i))
+        if height[i] < height[j] {
             i++
-        }else{
+        } else {
             j--
         }
     }
     return maxArea
-}
-
-func findMax( i, j  int) int{
-    if i < j {
-        return j
-    }else{
-        return i
-    }
-}
-
-func findMin( i, j  int) int{
-    if i < j {
-        return i
-    }else{
-        return j
-    }
 }
--- Pattern19/LC130/SurroundedRegions.go
+++ Pattern19/LC130/SurroundedRegions.go
@@ -1,2 +1,51 @@
 package LC130
 
+func solve(board [][]byte) {
+
+	rows := len(board)
+	cols := len(board[0])
+
+	var dfs func(int, int)
+
+	dfs = func(r int, c int) {
+		if r >= rows || c >= cols || r < 0 || c < 0 || board[r][c] != 'O' {
+			return
+		}
+
+		board[r][c] = 'T'
+
+		dfs(r+1, c)
+		dfs(r-1, c)
+		dfs(r, c+1)
+		dfs(r, c-1)
+
+	}
+	for i := 0; i < rows; i++ {
+		if board[i][0] == 'O' {
+			dfs(i, 0)
+		}
+		if board[i][cols-1] == 'O' {
+			dfs(i, cols-1)
+		}
+
+	}
+	for j := 0; j < cols; j++ {
+		if board[0][j] == 'O' {
+			dfs(0, j)
+		}
+		if board[rows-1][j] == 'O' {
+			dfs(rows-1, j)
+		}
+
+	}
+
+	for i := 0; i< rows ; i++{
+		for j := 0; j < cols; j++{
+			if board[i][j] == 'T'{
+				board[i][j] = 'O'
+			}else if board[i][j] == 'O'{
+				board[i][j] = 'X'
+			}
+		}
+	}
+}
